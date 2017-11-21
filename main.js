3d83a21a177768be60e64a7b872d86e0d16f8bca
Kat Reinhart <kat.reinhart@gmail.com>
2017-11-21 14:47:07 -0800

    Add some command line 💅

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
+#! /usr/local/bin/node
+
 const yargs = require('yargs')
+const figlet = require('figlet')
 const options = ['rock', 'paper', 'scissors']
 
 function rps() {
   const {entry} = yargs.argv
+  figlet('rock paper scissors', (err, data) => {
+    if(err) {
+      console.log('Something went wrong')
+      console.dir(err)
+      return
+    } else {
 
-  if(!options.includes(entry)) return
-
-  let rand = Math.floor(Math.random() * 3)
-  let computerChoice = options[rand]
-
-  console.log(`You chose ${entry}. Computer chose ${computerChoice}.`)
-  if((entry === 'rock' && computerChoice === 'paper') 
-  || (entry === 'paper' && computerChoice === 'scissors') 
-  || (entry === 'scissors' && computerChoice === 'rock')) {
-    console.log('Computer wins!')
-  } else {
-    console.log('You win!')
-  }
+      console.log(data)
+      
+      
+      if(!options.includes(entry)) {
+        console.log('Please include a --entry= argument with one of the following: rock, paper, scissors')
+        return
+      }
+    
+      let computerChoice = options[Math.floor(Math.random() * 3)]
+    
+      console.log(`You chose ${entry}. Computer chose ${computerChoice}.`)
+      if((entry === 'rock' && computerChoice === 'paper') 
+      || (entry === 'paper' && computerChoice === 'scissors') 
+      || (entry === 'scissors' && computerChoice === 'rock')) {
+        console.log('Computer wins!')
+      } else {
+        console.log('You win!')
+      }
+    }
+  })
 }
 
 rps()
